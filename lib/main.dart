da5b09abcebc79ad750c4be6d5adf3bf53f1c96f
SPablo2191 <pablosandoval2191@gmail.com>
2023-02-04 13:46:25 -0300

    feat: routes file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
+import 'src/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 import 'src/pages/alert_page.dart';
-import 'src/pages/home_page.dart';
-import 'src/pages/avatar_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,11 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Laboratory of components',
       // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage()
-      },
+      routes: getRoutes(),
       onGenerateRoute: (settings) {
         // se dispara cuando no esta en rutas
         return MaterialPageRoute(builder: (context) => AlertPage());
