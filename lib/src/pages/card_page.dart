b6342e3d871e4461ed847ef7954cd485f1e50c21
SPablo2191 <pablosandoval2191@gmail.com>
2023-02-04 14:16:42 -0300

    feat: Cards page added

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  const CardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: [_getCard1()],
+      ),
+    );
+  }
+
+  Widget _getCard1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text('Card 1'),
+            subtitle: Text('Soy la primera Tarjeta creada con Flutter yupi'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(onPressed: () {}, child: Text('Cancelar')),
+              TextButton(onPressed: () {}, child: Text('Elegir')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../pages/alert_page.dart';
 import '../pages/avatar_page.dart';
+import '../pages/card_page.dart';
 import '../pages/home_page.dart';
 
 Map<String, WidgetBuilder> getRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
-    'avatar': (BuildContext context) => AvatarPage()
+    'avatar': (BuildContext context) => AvatarPage(),
+    'card': (BuildContext context) => CardPage()
   };
 }
