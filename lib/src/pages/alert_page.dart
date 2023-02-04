2647f2fe4be2369288dd919cf97d6aa50bc5435b
SPablo2191 <pablosandoval2191@gmail.com>
2023-02-04 16:39:41 -0300

    feat: alert dialog added

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,51 @@ class AlertPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Alert')),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () => _showAlert(context),
+            child: Text('Mostrar Alerta'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.amber,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+            )),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(context);
         },
         child: Icon(Icons.keyboard_arrow_left),
       ),
+    );
+  }
+
+  _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text('Advertencia'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Buenas! este es un mensaje de alerta'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('Cancelar')),
+            ElevatedButton(onPressed: () {}, child: Text('Aceptar'))
+          ],
+        );
+      },
     );
   }
 }
