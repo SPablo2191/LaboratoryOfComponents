5620246e6f47e10d2c6d80d66bae1847d60194d4
SPablo2191 <pablosandoval2191@gmail.com>
2023-02-05 10:38:51 -0300

    feat: animated container working

--- lib/src/pages/animated_container_page.dart
+++ lib/src/pages/animated_container_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class AnimatedContainerPage extends StatefulWidget {
   const AnimatedContainerPage({super.key});
@@ -8,12 +9,44 @@ class AnimatedContainerPage extends StatefulWidget {
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animated Container'),
       ),
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(seconds: 1),
+          curve: Curves.bounceOut,
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+              borderRadius: _borderRadius,
+              color: _color,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, blurRadius: 10, offset: Offset(1, 1))
+              ]),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => _changeContainer(), child: Icon(Icons.play_arrow)),
     );
+  }
+
+  _changeContainer() {
+    final random = Random();
+    setState(() {
+      _color = Color.fromRGBO(
+          random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(300).toDouble();
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+    });
   }
 }
