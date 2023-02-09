04fd5296c0dcb57098078ae4f75aa4c756e51a11
SPablo2191 <pablosandoval2191@gmail.com>
2023-02-09 17:52:05 -0300

    feat: infinite scroll added

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -8,7 +8,21 @@ class ListViewPage extends StatefulWidget {
 }
 
 class _ListViewPageState extends State<ListViewPage> {
-  List<int> numbers = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+  List<int> _numbers = [];
+  int _lastItem = 0;
+  @override
+  void initState() {
+    super.initState();
+    _addTenItems();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _addTenItems();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,13 +35,21 @@ class _ListViewPageState extends State<ListViewPage> {
 
   _createListView() {
     return ListView.builder(
-        itemCount: numbers.length,
+        controller: _scrollController,
+        itemCount: _numbers.length,
         itemBuilder: (context, index) {
-          final image = numbers[index];
+          final image = _numbers[index];
           return FadeInImage(
               placeholder: AssetImage('assets/jar.gif'),
               image:
                   NetworkImage('https://picsum.photos/500/300/?image=$image'));
         }); // como se va a dibujar este componente
+  }
+
+  _addTenItems() {
+    for (var i = 0; i < 10; i++) {
+      _numbers.add(_lastItem++);
+    }
+    setState(() {});
   }
 }
